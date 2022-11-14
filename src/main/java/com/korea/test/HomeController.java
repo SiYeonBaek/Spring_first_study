383b3da749c19c05082c62d076af4d65ebf47684
SiYeonBaek <siyeon092700@gmail.com>
2022-11-14 15:09:42 +0900

    스프링 첫번째 실습

---
+++ src/main/java/com/korea/test/HomeController.java
@@ -0,0 +1,45 @@
+package com.korea.test;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		// 날짜를 구해서...
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		
+		// 요부분을 참조하고 있다.
+		model.addAttribute("serverTime", formattedDate );
+		
+		model.addAttribute("hello","spring에 오신것을 환영합니다.");
+		
+		return "home";	// jsp에서는 바인딩을 하면 포워딩을 해주는데 
+						// 그것 대신 addAttribute로 저장한 값을 home.jsp에 포워딩 한다는 의미
+	}
+	
+}
